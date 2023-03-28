7933514489564e34733dc83ad23aadfc7304d534
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2023-03-28 16:07:50 +0000

    [1.2.0] #0 - UPGRADE TO RJWEB-SERVER 5.7.0

--- lib/cjs/index.d.ts
+++ lib/cjs/index.d.ts
@@ -1,6 +1,16 @@
-import { HTTPRequestContext, Middleware } from "rjweb-server";
+import { HTTPRequestContext } from "rjweb-server";
 import ejs from "ejs";
-export declare function Init(defaultOptions?: ejs.Options): Middleware;
+declare const init: (config?: ejs.Options) => {
+    data: import("rjweb-server/lib/cjs/classes/middlewareBuilder").MiddlewareData<ejs.Options, {
+        options: ejs.Options;
+    }>;
+    config: ejs.Options;
+    version: number;
+    localContext: {
+        options: ejs.Options;
+    };
+};
+export { init };
 export interface Props {
     /** Print a Rendered EJS Template File to the Client */ printEJS: (file: string, data?: ejs.Data, options?: ejs.Options) => HTTPRequestContext;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,41 @@
-import { HTTPRequestContext, Middleware, MiddlewareToProps } from "rjweb-server"
+import { HTTPRequestContext, MiddlewareBuilder, MiddlewareToProps } from "rjweb-server"
 type HTTPRequestContextFull = HTTPRequestContext & MiddlewareToProps<[ Props ]>
 
 import path from "path"
 import ejs from "ejs"
 import fs from "fs"
 
-export function Init(defaultOptions: ejs.Options = {}): Middleware {
-	return {
-		name: 'rjweb-server-ejs',
+const { init } = new MiddlewareBuilder<ejs.Options, { options: ejs.Options }>()
+	.init((lCtx, config) => {
+		lCtx.options = config
+	})
+	.http((lCtx, stop, ctr: HTTPRequestContextFull, ctx) => {
+		ctr.printEJS = (file, data = {}, options = {}) => {
+			ctx.scheduleQueue('execution', () => new Promise<void>(async(resolve, reject) => {
+				const content = await fs.promises.readFile(path.resolve(file), 'utf8')
 
-		code: (ctr: HTTPRequestContextFull, ctx) => {
-			ctr.printEJS = (file, data = {}, options = {}) => {
-				ctx.scheduleQueue('execution', () => new Promise<void>(async(resolve, reject) => {
-					const content = await fs.promises.readFile(path.resolve(file), 'utf8')
+				try {
+					ctx.response.content = Buffer.from(await ejs.render(content, data, {
+						beautify: false,
+						root: path.dirname(file),
+						...lCtx.options,
+						...options,
+						async: true
+					}))
 
-					try {
-						ctx.response.content = Buffer.from(await ejs.render(content, data, {
-							beautify: false,
-							root: path.dirname(file),
-							...defaultOptions,
-							...options,
-							async: true
-						}))
+					ctr.setHeader('Content-Type', 'text/html')
+					resolve()
+				} catch (err) {
+					reject(err)
+				}
+			}))
 
-						ctr.setHeader('Content-Type', 'text/html')
-						resolve()
-					} catch (err) {
-						reject(err)
-					}
-				}))
-
-				return ctr
-			}
+			return ctr
 		}
-	}
-}
+	})
+	.build()
+
+export { init } 
 
 export interface Props {
 	/** Print a Rendered EJS Template File to the Client */ printEJS: (file: string, data?: ejs.Data, options?: ejs.Options) => HTTPRequestContext
