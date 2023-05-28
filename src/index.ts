a11fbd30c3a7e8d1825de844d8764ce594b645e0
robert <rjansengd@gmail.com>
2023-05-28 22:35:53 +0200

    [1.5.1] #0 - ADD ROOTFILEPATH OPTION

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,38 @@
-import { HttpRequest, MiddlewareBuilder } from "rjweb-server"
+import { MiddlewareBuilder } from "rjweb-server"
 
 import path from "path"
 import ejs from "ejs"
 import fs from "fs"
 
-export const eJS = new MiddlewareBuilder<ejs.Options, { config: ejs.Options }>()
+const handleRootFile = async(root: Options['root'] = '.', file: string): Promise<string> => {
+	if (typeof root === 'string') return path.join(root, file)
+	else {
+		let result = path.join(root[root.length - 1], file)
+
+		for (const rootPath in root) {
+			const realPath = path.join(rootPath, file)
+			try {
+				await fs.promises.access(realPath, fs.constants.R_OK)
+
+				result = realPath
+				break
+			} catch {
+				continue
+			}
+		}
+
+		return result
+	}
+}
+
+type Options = ejs.Options & {
+	/**
+	 * Apply root from config to initial file path
+	 * @default false
+	*/ rootFilePath?: boolean
+}
+
+export const eJS = new MiddlewareBuilder<Options, { config: Options }>()
 	.init((lCtx, config) => {
 		lCtx.config = config
 	})
@@ -15,24 +43,27 @@ export const eJS = new MiddlewareBuilder<ejs.Options, { config: ejs.Options }>()
 		 * @from rjweb-server-ejs
 		*/ public printEJS(file: string, data: ejs.Data = {}, options: ejs.Options = {}): this {
 			this.ctx.setExecuteSelf(() => new Promise(async(resolve, reject) => {
-				const content = await fs.promises.readFile(path.resolve(file), 'utf8')
-	
 				try {
-					this.ctx.response.content = await ejs.render(content, data, {
+					const config: ejs.Options = {
 						beautify: false,
 						root: path.dirname(file),
 						...lCtx.config,
 						...options,
+						rmWhitespace: true,
 						async: true
-					})
-	
+					}
+
+					let location = file
+					if (lCtx.config.rootFilePath) location = await handleRootFile(config.root, file)
+					this.ctx.response.content = await ejs.renderFile(location, data, config)
+
 					this.headers.set('Content-Type', 'text/html')
 					resolve(true)
 				} catch (err) {
 					reject(err)
 				}
 			}))
-	
+
 			return this
 		}
 	})
