49d16f00336a338438b0be05e910835ceff78620
robert <rjansengd@gmail.com>
2023-05-28 11:44:02 +0200

    [1.5.0] #0 - UPDATE TO RJWEB-SERVER 8.0.0

--- src/index.ts
+++ src/index.ts
@@ -4,41 +4,38 @@ import path from "path"
 import ejs from "ejs"
 import fs from "fs"
 
-class Http extends HttpRequest {
-	/**
-	 * Print an EJS File to the Client
-	 * @since 1.3.0
-	 * @from rjweb-server-ejs
-	*/ public printEJS(file: string, data: ejs.Data = {}, options: ejs.Options = {}): this {
-		this.ctx.setExecuteSelf(() => new Promise(async(resolve, reject) => {
-			const content = await fs.promises.readFile(path.resolve(file), 'utf8')
-
-			try {
-				this.ctx.response.content = await ejs.render(content, data, {
-					beautify: false,
-					root: path.dirname(file),
-					...globalOptions,
-					...options,
-					async: true
-				})
-
-				this.setHeader('Content-Type', 'text/html')
-				resolve(true)
-			} catch (err) {
-				reject(err)
-			}
-		}))
-
-		return this
-	}
-}
-
-let globalOptions: ejs.Options = {}
-export const eJS = new MiddlewareBuilder<{}, ejs.Options>()
+export const eJS = new MiddlewareBuilder<ejs.Options, { config: ejs.Options }>()
 	.init((lCtx, config) => {
-		globalOptions = config
+		lCtx.config = config
 	})
-	.httpClass(() => Http)
+	.httpClass((e, lCtx) => class Http extends e {
+		/**
+		 * Print an EJS File to the Client
+		 * @since 1.3.0
+		 * @from rjweb-server-ejs
+		*/ public printEJS(file: string, data: ejs.Data = {}, options: ejs.Options = {}): this {
+			this.ctx.setExecuteSelf(() => new Promise(async(resolve, reject) => {
+				const content = await fs.promises.readFile(path.resolve(file), 'utf8')
+	
+				try {
+					this.ctx.response.content = await ejs.render(content, data, {
+						beautify: false,
+						root: path.dirname(file),
+						...lCtx.config,
+						...options,
+						async: true
+					})
+	
+					this.headers.set('Content-Type', 'text/html')
+					resolve(true)
+				} catch (err) {
+					reject(err)
+				}
+			}))
+	
+			return this
+		}
+	})
 	.build()
 
 /** @ts-ignore */
